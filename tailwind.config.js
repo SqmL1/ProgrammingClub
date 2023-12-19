c30af8070356e3335b39fea91a2812b05851649b
Aervyon <aervy@aervyon.com>
2023-12-19 02:26:26 -0600

    feat: half-ass webpage & theme
    I updated the theme for the site and made a half-ass webpage under /aervy.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "header-text": "#006590",
+        "header-text-secondary": "#FFFFFF",
+        "text": "#000000",
+        "text-secondary": "#C9C9C9",
+        "bg-primary": "#FFFFFFFF",
+        "bg-secondary": "#000000",
+        "bg-tertiary": "#006590"
+      }
+    },
   },
   plugins: [],
 }
