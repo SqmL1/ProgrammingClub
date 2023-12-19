6020bec25579167a9692d8ea3d568103b6f18709
Aervyon <aervy@aervyon.com>
2023-12-19 02:39:32 -0600

    Updated the page again

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
         "text-secondary": "#C9C9C9",
         "bg-primary": "#FFFFFFFF",
         "bg-secondary": "#000000",
-        "bg-tertiary": "#006590"
+        "bg-tertiary": "#006590",
+        "link": "#0194D3"
       }
     },
   },
