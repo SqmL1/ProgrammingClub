0560fca1f74456e943f4058a79745b64ca80743f
Daren <darenbach2020@gmail.com>
2023-12-22 17:18:05 -0600

    Completed all page Transitions

--- components/navbar.vue
+++ components/navbar.vue
@@ -6,9 +6,11 @@
   <header>
     <nav class="flex py-2 bg-gray-700 dark:bg-black flex-wrap items-center justify-between mx-auto px-20">
       <!-- Logo as link-->
-      <a href="/" class="flex items-center rtl:space-x-revers text-blue-500">
+    <NuxtLink to="/">
+      <span class="flex items-center rtl:space-x-revers text-blue-500">
         <img src="/image/logo-bold.svg" alt="AR Coding Club logo">
-      </a>
+      </span>
+    </NuxtLink>
 
       <!-- This button expands the menu when on smaller displays -->
       <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
@@ -26,18 +28,24 @@
           </li>
           </NuxtLink>
 
+        <NuxtLink to="/calendar">
           <li>
-            <a href="/calendar" class="text-lg block py-2 px-3 text-text-secondary rounded md:border-0 md:hover:text-link md:p-0 font-mono">Calendar</a>
+            <span class="text-lg block py-2 px-3 text-text-secondary rounded md:border-0 md:hover:text-link md:p-0 font-mono">Calendar</span>
           </li>
+        </NuxtLink>
 
+        <NuxtLink to="/About">
           <li>
-            <a href="/About" class="text-lg block py-2 px-3 text-text-secondary rounded md:border-0 md:hover:text-link md:p-0 font-mono">About Us</a>
+            <span class="text-lg block py-2 px-3 text-text-secondary rounded md:border-0 md:hover:text-link md:p-0 font-mono">About Us</span>
           </li>
+        </NuxtLink>
           
+        <NuxtLink to="/join">
           <li>
             <!-- Need to do this later -->
             <a href="/join" class="text-lg block py-2 px-3 text-text-secondary rounded md:border-0 md:hover:text-link md:p-0 md:dark:hover:text-link font-mono">Join</a>
           </li>
+        </NuxtLink>
 
         </ul>
       </div>
--- pages/resources.vue
+++ pages/resources.vue
@@ -26,7 +26,7 @@ useHead({
 </script>
 
 <template>
-   <div class="w-fit text-lg">
+   <div class=" w-fit text-lg">
       <h1 class="max-w-md font-bold text-4xl text-header-text 2xl:w-prose w-fit">Resources</h1>
    </div>
 </template>
