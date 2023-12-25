02303a1e349d4ce6f550c7338e08f93a342a4d4f
Daren <darenbach2020@gmail.com>
2023-12-25 04:09:04 -0600

    fixed a bug.

--- pages/resources.vue
+++ pages/resources.vue
@@ -26,9 +26,10 @@ useHead({
 </script>
 
 <template>
-   
-<h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Collaborate!</span> We Provide Resources.</h1>
-<p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 text-center">We believe that our club will provide resources and opputunities to help eachother grow!</p>
+  <div>
+   <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Collaborate!</span> We Provide Resources.</h1>
+   <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 text-center">We believe that our club will provide resources and opputunities to help eachother grow!</p>
+  </div> 
 
 </template>
 
