d666249b0864039de98d1a7f1617ba0599e0d012
Aervyon <aervy@aervyon.com>
2023-12-30 20:05:08 -0600

    feat(events.json:: Begin working on events page

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,4 +1,39 @@
 <script setup lang="ts">
+import eventsJson from '../events.json';
+
+for (const eventie in eventsJson) {
+  console.log(eventie);
+}
+const events = reactive(eventsJson);
+
+let year = new Date().getFullYear();
+let month = new Date().getMonth();
+
+const months = [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december"
+]
+
+if (year < 2024) {
+  year = 2024;
+  month = 0;
+}
+
+console.log(month);
+console.log(year);
+
+const monthWord = months[month];
+console.log(monthWord)
 
 useSeoMeta({
    description: 'A calendar of events for the Anoka-Ramsey Programming Club',
@@ -21,12 +56,22 @@ useHead({
          type: 'image/png',
          href: '/favicon.png'
       }
-   ]
+   ],
+  script: [
+    {
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/datepicker.min.js'
+    }
+  ]
 })
 </script>
 
 <template>
-
+  <ul>
+    <li v-for="event in events[year][monthWord]" :key="event">
+      <!-- Month -->
+      <span v-for="item in event" :key="item">{{item}}</span>
+    </li>
+  </ul>
 </template>
 
 <style scoped>
