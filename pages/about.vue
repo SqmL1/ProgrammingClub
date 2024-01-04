9531be0b154756a2ad8567e04c8bd83dc453a21f
Daren <darenbach2020@gmail.com>
2024-01-04 14:14:07 -0600

    do we keep the buttons?

--- pages/about.vue
+++ pages/about.vue
@@ -22,22 +22,23 @@
                             <!-- Dropdown menu -->
                             <div id="dropdown" class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                                 <ul class="py-2" aria-labelledby="dropdownButton">
-                                <li>
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Export Data</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
-                                </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Export Data</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
                         <div class="flex flex-col items-center pb-10">
                             <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="" alt=""/>
-                            <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Daren</h5>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                            <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Daren Bach</h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Student/SWE</span>
+                            <span class="text-center mt-2">Hi! I'm Daren, but you can also call me "The Boulder". I am an aspiring Software Engineer!</span>
                             <div class="flex mt-4 md:mt-6">
                                 <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a>
                                 <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Message</a>
@@ -119,7 +120,7 @@
                 </div>
             </div>
 
-            <NuxtLink to="/about">
+                <NuxtLink to="/about">
                     <div class="p-1.5 bg-gradient-to-tl to-blue-400 from-yellow-300 w-fit rounded-2xl mx-auto mt-10">
                     <button class="bg-white text-black text-2xl position left-6 right-6 w-fit max-w-fit p-3 rounded-xl font-bold ">Join Now!</button>
                     </div>
