ca889d85e243b864d8e392f5acde8cd3001423fe
SqmL1 <samuilIordachi@gmail.com>
2024-01-04 11:46:37 -0600

    aboutBase

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,41 @@
 <template>
-    <div>
-        <h1></h1>
-
-    <NuxtLink to="/about">
-        <div class="p-1.5 bg-gradient-to-tl to-blue-400 from-yellow-300 w-fit rounded-2xl ml-16 mt-4">
-        <button class="bg-white text-black text-2xl position left-6 right-6 w-fit max-w-fit p-3 rounded-xl font-bold ">Join Now!</button>
+    
+    <div class="container mx-auto grid grid-cols-4 grid-rows-4 gap-6 mt-10">
+        <div class="item col-span-3   ">
+            <h1 class="font-bold text-5xl text-sky-800">What?</h1>
+            <div class="rounded-lg border border-black bg-white p-4 shadow-md w-2/3"> <p>Our club is designed to be all about creating a sense of community in Anoka Ramsey especially in </p>
         </div>
-    </NuxtLink>
+        
+        </div>
+
+        <div class="item col-span-3 text-sky-800">
+            <h1 class="font-bold text-5xl">When?</h1>
+            <div class="rounded-lg border border-black bg-white p-4 shadow-md w-2/3"> 
+                <p>
+
+                </p>
+            </div> 
+        </div>
+
+        <div class="item col-span-3 text-sky-800 ">
+            <h1 class="font-bold text-5xl text-sky-800">Where?</h1>
+                <div class="rounded-lg border border-black bg-white p-4 shadow-md w-2/3">
+                    <p > </p>
+                </div>
+        </div>
+
+        <div class="item col-span-3 text-sky-800 ">
+             <h1 class="font-bold text-5xl">Why?</h1>
+                <div class="rounded-lg border border-black bg-white p-4 shadow-md w-2/3"> 
+                    <p></p>
+                </div>
+        </div>
     </div>
+
+
+    
+
+    
 
 
 </template>
