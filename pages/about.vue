77f926477e228db4a0315e90d9bffdfe916390a9
Daren <darenbach2020@gmail.com>
2024-01-03 21:32:28 -0600

    added some backround stuff.

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,16 @@
 <template>
-    <div>
-        <h1></h1>
+    <div class="flex justify-center">
+        <div class="bg-zinc-800 h-screen w-3/4">
+            <div>
+                <h1></h1>
 
-    <NuxtLink to="/about">
-        <div class="p-1.5 bg-gradient-to-tl to-blue-400 from-yellow-300 w-fit rounded-2xl ml-16 mt-4">
-        <button class="bg-white text-black text-2xl position left-6 right-6 w-fit max-w-fit p-3 rounded-xl font-bold ">Join Now!</button>
+            <NuxtLink to="/about">
+                <div class="p-1.5 bg-gradient-to-tl to-blue-400 from-yellow-300 w-fit rounded-2xl ml-16 mt-4">
+                <button class="bg-white text-black text-2xl position left-6 right-6 w-fit max-w-fit p-3 rounded-xl font-bold ">Join Now!</button>
+                </div>
+            </NuxtLink>
+            </div>
         </div>
-    </NuxtLink>
     </div>
-
 
 </template>
