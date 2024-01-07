667fea6d008150ca177192549e9ab6bb20bcddac
Aervyon <aervy@aervyon.com>
2024-01-06 21:21:52 -0600

    fix(navbar): Make navbar open properly

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 
+const navbarOpen = ref(false)
+
+function toggleNavbar() {
+   navbarOpen.value = !navbarOpen.value;
+}
+
 </script>
 
 <template>
@@ -13,14 +19,20 @@
     </NuxtLink>
 
       <!-- This button expands the menu when on smaller displays -->
-      <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      <button
+            v-on:click="toggleNavbar"
+            data-collapse-toggle="navbar-default"
+            type="button"
+            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
       </button>
       <!-- Links & stuff -->
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <div class="w-full md:block md:w-auto" id="navbar-default" :class="{
+         'hidden': !navbarOpen
+      }">
         <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-16 rtl:space-x-reverse md:mt-0 md:border-0 dark:border-black">
             <NuxtLink to="/resources">
                <li>
