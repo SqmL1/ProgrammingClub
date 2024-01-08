d53c7ac0820637dfbcca946649a09fe67d2f5d05
Aervyon <aervy@aervyon.com>
2024-01-07 18:22:14 -0600

    fix(InternalLink): Fixed roundness of the internal link component, it is now sufficiently round

--- components/InternalLink.vue
+++ components/InternalLink.vue
@@ -9,7 +9,7 @@ defineProps<{
    <div class="w-fit relative rounded-2xl">
       <NuxtLink :to="link" class="relative w-fit rounded-2xl h-fit bg-white hover:bg-gray-100">
          <div
-             class="absolute -inset-1 rounded-2xl bg-gradient-to-tl from-yellow-300 to-link opacity-75 blur"
+             class="absolute -inset-1 bg-gradient-to-tl rounded-full from-yellow-300 to-link opacity-75 blur"
          ></div>
          <div class="relative p-1.5 w-fit rounded-full bg-inherit">
             <p class="text-black text-2xl p-3 rounded-full font-bold font-mono bg-inherit">{{text}}</p>
