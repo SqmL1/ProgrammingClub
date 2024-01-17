88f633cc02322376078390f0ed44637213cd411e
SqmL1 <samuilIordachi@gmail.com>
2024-01-16 23:33:45 -0600

    Added: Sam About Links + PFP

--- pages/about.vue
+++ pages/about.vue
@@ -35,8 +35,8 @@
                             <span class="text-sm text-gray-500 dark:text-gray-400">Student/SWE</span>
                             <span class="text-center mt-2 mx-2 max-w-prose">Hello, my name is Sam and I am student at Anoka Ramsey and an aspiring software engineer.</span>
                             <div class="flex mt-4 md:mt-6">
-                                <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-header-text rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a>
-                                <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Message</a>
+                                <a href="https://www.instagram.com/sqmxxl/" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-header-text rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a>
+                                <a href="mailto:samuilIordachi@gmail.com" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Message</a>
                             </div>
                         </div>
                     </div>
