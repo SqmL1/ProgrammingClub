58f053227ae5ab35e155383d3d96e64fe6fcb6a9
Aervyon <aervy@aervyon.com>
2025-01-13 15:18:47 -0600

    fix(links): Links now display fancily across Firefox and Chrome

--- components/InternalLink.vue
+++ components/InternalLink.vue
@@ -6,21 +6,21 @@
 </script>
 
 <template>
-	<div class="w-fit relative rounded-2xl">
 		<NuxtLink
 			:to="link"
-			class="relative w-fit rounded-2xl h-fit bg-white hover:bg-gray-100"
+			class="relative w-fit h-fit"
 		>
+			<div class="relative">
 			<div
-				class="absolute -inset-1 bg-gradient-to-tl rounded-full from-yellow-300 to-link opacity-75 blur"
+				class="absolute -inset-1 rounded-full bg-gradient-to-tl from-yellow-300 to-link opacity-75 blur"
 			></div>
-			<div class="relative p-1.5 w-fit rounded-full bg-inherit">
-				<p
-					class="text-black text-lg md:text-2xl p-3 rounded-full font-bold bg-inherit font-extra"
+				<div
+				class="relative block max-w-lg p-6 bg-white rounded-full hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700"
 				>
-					{{ text }}
-				</p>
+					<div class="font-bold font-extra">
+						{{ text }}
+					</div>
+				</div>
 			</div>
 		</NuxtLink>
-	</div>
 </template>
