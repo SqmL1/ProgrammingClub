39ad4b34e1f6aac50409a4b17ac5273628c97801
Aervyon <aervy@aervyon.com>
2025-01-13 15:53:18 -0600

    fix(components/InternalLink.vue): Fix link container being too long

--- components/InternalLink.vue
+++ components/InternalLink.vue
@@ -8,16 +8,16 @@
 <template>
 		<NuxtLink
 			:to="link"
-			class="relative w-fit h-fit"
+			class="relative flex flex-shrink"
 		>
 			<div class="relative">
 			<div
 				class="absolute -inset-1 rounded-full bg-gradient-to-tl from-yellow-300 to-link opacity-75 blur"
 			></div>
 				<div
-				class="relative block max-w-lg p-6 bg-white rounded-full hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700"
+				class="relative block max-w-lg py-4 px-6 bg-white rounded-full hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 w-fit"
 				>
-					<div class="font-bold font-extra">
+					<div class="font-bold font-extra w-fit">
 						{{ text }}
 					</div>
 				</div>
